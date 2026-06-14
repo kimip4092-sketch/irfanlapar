24196c9e438230e22d6bf6113dc2a5d6829c18ee
kimip4092-sketch <kimip4092@gmail.com>
2026-06-14 13:34:38 +0800

    Add a new token to token.js

--- token.js
+++ token.js
@@ -1,3 +1,3 @@
 {
-  "tokens": ["8314265827:AAHTv_LwozO87GVDAa8WScFiceUqTSKYtQk"],[""]
+  "tokens": ["8314265827:AAHTv_LwozO87GVDAa8WScFiceUqTSKYtQk"],["8851825191:AAGU_cuT4rWmc1cb1zjb320V8gPQcVU99VM"]
 }
